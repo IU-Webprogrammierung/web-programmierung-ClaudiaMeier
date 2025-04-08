06b4054c1da49feba0e4aa47289db4bb7ff96759
Claudia Meier <93claudia7@gmail.com>
2025-04-09 05:27:29 +0900

    added back to top button, changed script to end of body instead of head

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//NAVIGATION 
 function showSidebar(){
    const sidebar = document.querySelector(".sidebar")
    sidebar.style.display = "flex"
@@ -5,4 +7,33 @@ function showSidebar(){
 function hideSidebar() {
    const sidebar = document.querySelector(".sidebar")
    sidebar.style.display = "none"
+}
+
+document.addEventListener("click", function(event) {
+   const sidebar = document.querySelector(".sidebar");
+   const menuButton = document.querySelector(".menu_btn");
+   
+   if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+       hideSidebar();
+   }
+});
+
+
+//BACK TO TOP BUTTON
+let mybutton = document.getElementById("back_btn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
