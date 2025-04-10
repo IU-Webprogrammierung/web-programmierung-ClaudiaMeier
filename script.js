10d0e1aea60e49e36d7cb17935f33e3296cc0982
Claudia Meier <93claudia7@gmail.com>
2025-04-11 04:01:19 +0900

    form validation for name and mail in contact form

--- script.js
+++ script.js
@@ -38,3 +38,105 @@ function topFunction() {
    behavior: "smooth"
   });
 }
+
+//FORM VALIDATION
+
+let regMail=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+let inputName=document.querySelector('#name');
+let inputMail=document.querySelector('#email');
+let inputSubmit=document.querySelector('[type=submit]');
+
+let allErrorDiv=document.querySelectorAll(".errorDiv");
+
+let errorMessageName=[];
+let errorMessageMail=[];
+
+inputName.addEventListener("input",checkInputName);
+inputMail.addEventListener("input",checkInputMail);
+
+document.forms[0].addEventListener("submit",checkInput);
+
+function checkInput(e){
+  e.preventDefault();
+  checkInputName();
+  checkInputMail();
+}
+
+function checkInputName() {
+
+  let errorName=[];
+
+  errorMessageName.length=0;
+
+  errorName.push(checkEmpty(inputName,errorMessageName));
+  errorName.push(checkMinMax(inputName,3,100,errorMessageName));
+
+  if(errorName.includes(false)){
+    inputName.classList.add("error")
+    allErrorDiv[0].textContent=errorMessageName.join("");
+  }
+  else {
+    inputName.classList.remove("error")
+    allErrorDiv[0].textContent="";
+  }
+}
+
+function checkInputMail() {
+
+  let errorMail=[];
+
+  errorMessageMail.length=0;
+
+  errorMail.push(checkEmpty(inputMail,errorMessageMail));
+  errorMail.push(checkMinMax(inputMail,3,100,errorMessageMail));
+  errorMail.push(checkMail(inputMail,errorMessageMail));
+
+  if(errorMail.includes(false)){
+    inputMail.classList.add("error")
+    allErrorDiv[1].textContent=errorMessageMail.join("");
+  }
+  else {
+    inputMail.classList.remove("error")
+    allErrorDiv[1].textContent="";
+  }
+}
+
+function checkMinMax(inp,min,max,arrayMessage){
+  if(inp.value.length<min){
+    arrayMessage[1]="Mindestens "+min+" Zeichen\n"
+    return false;
+  }
+  else if(inp.value.length>max){
+    arrayMessage[2]="Maximal "+max+" Zeichen\n"
+    return false;
+  }
+  else {
+    arrayMessage[1]="";
+    arrayMessage[2]="";
+    return true;
+  }
+}
+
+function checkEmpty(inp,arrayMessage){
+
+  if(inp.value.trim()==="") {
+    arrayMessage[0]="Das ist ein Pflichtfeld\n";
+    return false;
+  }
+  else {
+    arrayMessage[0]="";
+    return true;
+  }
+}
+
+function checkMail(inp,arrayMessage){
+  if(!regMail.test(inp.value)){
+    arrayMessage[3]="Format ungültig\n";
+    return false;
+  }
+  else {
+    arrayMessage[3]="";
+    return true;
+  }
+}
