5c1eda8e5ed155492004035e2e63ed2b80fabd0a
Claudia Meier <93claudia7@gmail.com>
2025-04-12 01:18:02 +1000

    lazy load for all img, changed color from social media links and submit button, checked js script

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 
 //NAVIGATION 
-function showSidebar(){
-   const sidebar = document.querySelector(".sidebar")
-   sidebar.style.display = "flex"
+function showSidebar() {
+  let sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
 function hideSidebar() {
-   const sidebar = document.querySelector(".sidebar")
-   sidebar.style.display = "none"
+  let sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
 
 document.addEventListener("click", function(event) {
-   const sidebar = document.querySelector(".sidebar");
-   const menuButton = document.querySelector(".menu_btn");
-   
-   if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
-       hideSidebar();
-   }
+  let sidebar = document.querySelector(".sidebar");
+  let menuButton = document.querySelector(".menu_btn");
+  
+  if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+      hideSidebar();
+  }
 });
 
 
 //BACK TO TOP BUTTON
-let mybutton = document.getElementById("back_btn");
+let back_btn = document.getElementById("back_btn");
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+ if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  back_btn.style.display = "block";
+ } else {
+  back_btn.style.display = "none";
+ }
 }
 
 function topFunction() {
-  window.scrollTo({
-   top: 0,
-   behavior: "smooth"
-  });
+ window.scrollTo({
+  top: 0,
+  behavior: "smooth"
+ });
 }
 
 //FORM VALIDATION
@@ -45,7 +45,6 @@ let regMail=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 let inputName=document.querySelector('#name');
 let inputMail=document.querySelector('#email');
-let inputSubmit=document.querySelector('[type=submit]');
 
 let allErrorDiv=document.querySelectorAll(".errorDiv");
 
@@ -73,11 +72,11 @@ function checkInputName() {
   errorName.push(checkMinMax(inputName,3,100,errorMessageName));
 
   if(errorName.includes(false)){
-    inputName.classList.add("error")
+    inputName.classList.add("error");
     allErrorDiv[0].textContent=errorMessageName.join("");
   }
   else {
-    inputName.classList.remove("error")
+    inputName.classList.remove("error");
     allErrorDiv[0].textContent="";
   }
 }
@@ -93,22 +92,22 @@ function checkInputMail() {
   errorMail.push(checkMail(inputMail,errorMessageMail));
 
   if(errorMail.includes(false)){
-    inputMail.classList.add("error")
+    inputMail.classList.add("error");
     allErrorDiv[1].textContent=errorMessageMail.join("");
   }
   else {
-    inputMail.classList.remove("error")
+    inputMail.classList.remove("error");
     allErrorDiv[1].textContent="";
   }
 }
 
 function checkMinMax(inp,min,max,arrayMessage){
   if(inp.value.length<min){
-    arrayMessage[1]="Mindestens "+min+" Zeichen\n"
+    arrayMessage[1]="Mindestens "+min+" Zeichen\n";
     return false;
   }
   else if(inp.value.length>max){
-    arrayMessage[2]="Maximal "+max+" Zeichen\n"
+    arrayMessage[2]="Maximal "+max+" Zeichen\n";
     return false;
   }
   else {
